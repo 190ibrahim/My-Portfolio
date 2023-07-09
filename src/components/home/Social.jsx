11b8768dae8a8ee60ba06521631dfeaa14d8691b
190ibrahim <190ibrahimahmed@gmail.com>
2023-07-09 21:35:53 +0200

    Implemented: Home Section

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,28 @@
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="" className="home__social-icon" target="_blank" rel="noreferrer">
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/ibrahim-ibrahim-937a7823a/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://github.com/190ibrahim"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+}
+
+export default Social
