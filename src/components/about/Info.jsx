77a79e6805aeb83ea55391cf165f3ad9cfba3ac9
190ibrahim <190ibrahimahmed@gmail.com>
2023-07-10 04:32:13 +0200

    Completed: About Me Section

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,26 @@
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i className="bx bxs-graduation about__icon"></i>
+        <h3 className="about__title">Currently in </h3>
+        <span className="about__subtitle">4th Semester </span>
+      </div>
+
+      <div className="about__box">
+        <i className="bx bxs-badge-check about__icon"></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">18 + Projects</span>
+      </div>
+
+      <div className="about__box">
+        <i className="bx bxs-location-plus about__icon"></i>
+        <h3 className="about__title">Mobility </h3>
+        <span className="about__subtitle">Open to relocation</span>
+      </div>
+    </div>
+  );
+}
+
+export default Info
