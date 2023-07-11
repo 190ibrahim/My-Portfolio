e0ae95bd80a29cc2156614c6b0250cfc155e8cb5
190ibrahim <190ibrahimahmed@gmail.com>
2023-07-11 05:47:40 +0200

    Refactoring: Portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./portfolio.css";
+import PortfolioData from "./portfolioData ";
 
 const Portfolio = () => {
   const [toggleState, setToggleState] = useState(0);
@@ -14,214 +15,54 @@ const Portfolio = () => {
       <span className="section__subtitle">Most Recent Work</span>
 
       <div className="services__container container grid">
-        <div className="services__content">
-          <div>
-            <i className="uil uil-web-grid services__icon"></i>
-            <h3 className="services__title">
-              Content Management <br /> System
-            </h3>
-          </div>
+        {PortfolioData.map((item, index) => (
+          <div className="services__content" key={index}>
+            <div>
+              <i className={`uil ${item.icon} services__icon`}></i>
+              <h3
+                className="services__title"
+                dangerouslySetInnerHTML={{ __html: item.title }}
+              ></h3>
+            </div>
 
-          <span className="services__button" onClick={() => toggleTab(1)}>
-            More details
-            <i className="uil uil-arrow-right services__button-icon"></i>
-          </span>
+            <span
+              className="services__button"
+              onClick={() => toggleTab(index + 1)}
+            >
+              More details
+              <i className="uil uil-arrow-right services__button-icon"></i>
+            </span>
 
-          <div
-            className={
-              toggleState === 1
-                ? "services__modal active-modal"
-                : "services__modal"
-            }
-          >
-            <div className="services__modal-content">
-              <i
-                onClick={() => toggleTab(0)}
-                className="uil uil-times services__modal-close"
-              ></i>
+            <div
+              className={
+                toggleState === index + 1
+                  ? "services__modal active-modal"
+                  : "services__modal"
+              }
+            >
+              <div className="services__modal-content">
+                <i
+                  onClick={() => toggleTab(0)}
+                  className="uil uil-times services__modal-close"
+                ></i>
 
-              <h3 className="services__modal-title">
-                content management system
-              </h3>
-              <p className="services__modal-description">
-                A CMS is a web application that allow users to easily create,
-                edit, and publish content on a website, and it has the following
-                features:
-              </p>
+                <h3 className="services__modal-title">{item.project}</h3>
+                <p className="services__modal-description">
+                  {item.description}
+                </p>
 
-              <ul className="services__modal-services grid">
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    User authentication and authorization.
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    WYSIWYG (What You See Is What You Get)
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    CRUD operations for users and posts
-                  </p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Admin dashboard</p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Image uploading and management
-                  </p>
-                </li>
-              </ul>
+                <ul className="services__modal-services grid">
+                  {item.features.map((feature, featureIndex) => (
+                    <li className="services__modal-service" key={featureIndex}>
+                      <i className="uil uil-check-circle services__modal-icon"></i>
+                      <p className="services__modal-info">{feature}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="services__content">
-          <div>
-            <i className="uil uil-arrow services__icon"></i>
-            <h3 className="services__title">
-              Ui/Ux <br /> Designer
-            </h3>
-          </div>
-
-          <span onClick={() => toggleTab(2)} className="services__button">
-            More details
-            <i className="uil uil-arrow-right services__button-icon"></i>
-          </span>
-
-          <div
-            className={
-              toggleState === 2
-                ? "services__modal active-modal"
-                : "services__modal"
-            }
-          >
-            <div className="services__modal-content">
-              <i
-                onClick={() => toggleTab(0)}
-                className="uil uil-times services__modal-close"
-              ></i>
-
-              <h3 className="services__modal-title">Ui/Ux Designer</h3>
-              <p className="services__modal-description">
-                Service with more than 3 years of experience.Providing quality
-                work to clients and companies.
-              </p>
-
-              <ul className="services__modal-services grid">
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    I develop the user interface.
-                  </p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Web page development.</p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    I create ux element interactions.
-                  </p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    I position your company brand.
-                  </p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Design and mockups of products for companies.
-                  </p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <div className="services__content">
-          <div>
-            <i className="uil uil-edit services__icon"></i>
-            <h3 className="services__title">
-              Visual <br /> Designer
-            </h3>
-          </div>
-
-          <span onClick={() => toggleTab(3)} className="services__button">
-            More details
-            <i className="uil uil-arrow-right services__button-icon"></i>
-          </span>
-
-          <div
-            className={
-              toggleState === 3
-                ? "services__modal active-modal"
-                : "services__modal"
-            }
-          >
-            <div className="services__modal-content">
-              <i
-                onClick={() => toggleTab(0)}
-                className="uil uil-times services__modal-close"
-              ></i>
-
-              <h3 className="services__modal-title">Visual Designer</h3>
-              <p className="services__modal-description">
-                Service with more than 3 years of experience.Providing quality
-                work to clients and companies.
-              </p>
-
-              <ul className="services__modal-services grid">
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    I develop the user interface.
-                  </p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Web page development.</p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    I create ux element interactions.
-                  </p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    I position your company brand.
-                  </p>
-                </li>
-
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">
-                    Design and mockups of products for companies.
-                  </p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
---
+++ src/components/portfolio/portfolioData .jsx
@@ -0,0 +1,59 @@
+let PortfolioData = [
+  {
+    title: "Content<br />Management System",
+    project: "Content Management System",
+    description:
+      "A CMS is a web application that allows users to easily create, edit, and publish content on a website.",
+    features: [
+      "User authentication and authorization",
+      "WYSIWYG (What You See Is What You Get)",
+      "CRUD operations for users and posts",
+      "Admin dashboard",
+      "Image uploading and management",
+    ],
+    icon: "uil-web-grid",
+  },
+  {
+    title: "Trip4Life<br />App",
+    project: "Content Management System",
+    description:
+      "Trip4Life is a web-based application that allows users to plan and book sailing trips to various destinations.",
+    features: [
+      "User-friendly interface for browsing and booking trips",
+      "Secure user login and registration system",
+      "Built with PHP, JavaScript, and Bootstrap",
+      "User can view, edit, and delete their own bookings",
+      "Admin panel for managing destinations, boats, trips, and bookings",
+      "Responsive design for optimal viewing on different devices",
+      "The application uses prepared statements to prevent SQL injection attacks",
+    ],
+    icon: "uil-compass",
+  },
+  {
+    title: "EventifyUP<br />App",
+    project: "EventifyUP",
+    description: `A team Project where me and other three students collaborated to build this EventifyUP, 
+      a web-based platform designed to simplify the event ticketing process and provide a convenient
+      solution for users to register and purchase tickets for events at the University of Pecs. By
+      leveraging modern technologies, EventifyUp eliminates the need for traditional paper tickets
+      and offers a streamlined and secure digital ticketing experience.`,
+    features: [
+      "Built with Laravel and Angular",
+      "User Registration & athuntication",
+      "Unit testing",
+      "Calender integaration to dispaly all the events",
+      "Event Search Functionality",
+      "Admin Dashboard to implement CRUD on users and events",
+      "Event Listings: Comprehensive list of upcoming events with event details.",
+      "Event Registration: Simple and intuitive event registration process.",
+      "Mobile-Friendly Design: Responsive and mobile-friendly interface.",
+      "In progress: Secure Payment Integration: Integration of a reliable and secure payment gateway",
+      "In progress: Email Notifications: Automated email notifications throughout the ticketing process, including registration confirmation and delivery of QR codes.",
+      "In progress: Multilingual Support",
+    ],
+    icon: "uil-ticket",
+  },
+];
+
+
+export default PortfolioData ;
